ac807f9189b2059e8bd2a1e6f6cba50b5d9ab19d
Loris Sauter <loris.sauter@unibas.ch>
2020-07-29 10:27:33 +0200

    WiP: Experimental OpenAPI generated API

---
+++ Assets/CineastUnityInterface/Runtime/Vitrivr/UnityInterface/CineastApi/ExperimentalApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Utils;
+using Org.Vitrivr.Cineast.Api.Api;
+using Org.Vitrivr.Cineast.Api.Client;
+using Org.Vitrivr.Cineast.Api.Model;
+using UnityEngine;
+
+namespace CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi
+{
+  public class ExperimentalApi
+  {
+    private CineastConfiguration config;
+
+    private ObjectApi objectApi;
+
+    public ExperimentalApi(CineastConfiguration config)
+    {
+      this.config = config;
+    }
+
+    private void InitApis()
+    {
+      Configuration.Default.BasePath = config.cineastHost;
+      objectApi = new ObjectApi(Configuration.Default);
+    }
+
+    public MediaObjectQueryResult ListAllObjects()
+    {
+      return objectApi.FindObjectsAll("unused");
+    }
+  }
+}
