4f434ee986921c8f6d78ff2e40e83204a340d462
Florian Spiess <florian.spiess@unibas.ch>
2020-08-28 11:12:42 +0200

    Updated publish script to force pushes, properly output whether a publish was successful and allow specification of source branch.

--- publish.py
+++ publish.py
@@ -8,19 +8,29 @@ $> python publish.py --help
 Thanks to https://github.com/neogeek/unity-package-example for providing an excellent example
 """
 import argparse
-import os
+import subprocess
+import sys
 
 PACKAGE_DIR = 'Assets/CineastUnityInterface'
 
 parser = argparse.ArgumentParser(description="Utility to publish the stand-alone unity package available for upm.")
 
 parser.add_argument("-r", "--release", help="The name of the release branch", default="release")
-
+parser.add_argument("-b", "--branch", help="The name of the source branch (current if not specified)")
 
 args = parser.parse_args()
 
-cmd = f'git subtree push -f --prefix "{PACKAGE_DIR}" origin {args.release}'
+if args.branch:
+  branch = args.branch
+else:
+  result = subprocess.run(['git', 'branch', '--show-current'], stdout=subprocess.PIPE)
+  branch = result.stdout.decode().strip()
 
-os.system(cmd)
+result = subprocess.run(['git', 'subtree', 'split', '--prefix', f'{PACKAGE_DIR}', branch], stdout=subprocess.PIPE)
 
-print('Published!')
+result = subprocess.run(['git', 'push', 'origin', f'{result.stdout.decode().strip()}:{args.release}', '--force'])
+
+if result.returncode == 0:
+  print('Published!')
+else:
+  print('ERROR: Could not publish!', file=sys.stderr)
