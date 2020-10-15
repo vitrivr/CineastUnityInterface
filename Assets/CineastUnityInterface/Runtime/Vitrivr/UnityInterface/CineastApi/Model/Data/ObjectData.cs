72db927e6afd86c4f7c3788580a56d89f71cab0d
Florian Spiess <florianms@icloud.com>
2020-10-15 09:48:51 +0200

    Updated registry objects namespace and minor refactoring.

--- Assets/CineastUnityInterface/Runtime/Vitrivr/UnityInterface/CineastApi/Model/Data/ObjectData.cs
+++ Assets/CineastUnityInterface/Runtime/Vitrivr/UnityInterface/CineastApi/Model/Data/ObjectData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Model.Registries;
 using Org.Vitrivr.CineastApi.Model;
 using UnityEngine;
 
--- Assets/CineastUnityInterface/Runtime/Vitrivr/UnityInterface/CineastApi/Model/Data/SegmentData.cs
+++ Assets/CineastUnityInterface/Runtime/Vitrivr/UnityInterface/CineastApi/Model/Data/SegmentData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using CineastUnityInterface.Runtime.Vitrivr.UnityInterface.CineastApi.Model.Registries;
 using Org.Vitrivr.CineastApi.Model;
 using UnityEngine;
 
